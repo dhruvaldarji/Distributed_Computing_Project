8f6dd995318f097833c0a94c9d1fa6512038c0f3
Deion Anthony <dantho13@students.kennesaw.edu>
2015-10-03 14:36:32 -0400

    Edited the TCPSever class. Changed all the variables to private statics. Moved the code out of the main method and into a separate method called run. Call the run method from the main class. commented out the system exit method.

--- TCPServer.java
+++ TCPServer.java
@@ -7,16 +7,33 @@ public class TCPServer {
     private static int PORT = 5556;
     private static String CLIENT_ADDRESS = "10.99.26.144";
 
+    // Variables for setting up connection and communication
+    private static Socket Socket; // socket to connect with ServerRouter
+    private static PrintWriter out; // for writing to ServerRouter
+    private static BufferedReader in; // for reading form ServerRouter
+    private static InetAddress addr;
+    private static String host; // Server machine's IP
+    private static String routerName; // ServerRouter host name
+    private static int SockNum; // port number
+
     public static void main(String[] args) throws IOException {
 
+        try {
+            run();
+        } catch (Exception e) {
+            System.err.println("SOMETHING BROKE!\n" + e);
+        }
+    }
+
+    private static void run() throws IOException {
         // Variables for setting up connection and communication
-        Socket Socket = null; // socket to connect with ServerRouter
-        PrintWriter out = null; // for writing to ServerRouter
-        BufferedReader in = null; // for reading form ServerRouter
-        InetAddress addr = InetAddress.getLocalHost();
-        String host = addr.getHostAddress(); // Server machine's IP
-        String routerName = SERVER_ROUTER_NAME; // ServerRouter host name
-        int SockNum = PORT; // port number
+        Socket = null; // socket to connect with ServerRouter
+        out = null; // for writing to ServerRouter
+        in = null; // for reading form ServerRouter
+        addr = InetAddress.getLocalHost();
+        host = addr.getHostAddress(); // Server machine's IP
+        routerName = SERVER_ROUTER_NAME; // ServerRouter host name
+        SockNum = PORT; // port number
 
         // Tries to connect to the ServerRouter
         try {
@@ -59,6 +76,7 @@ public class TCPServer {
         in.close();
         Socket.close();
 
-        System.exit(0);
+        //System.exit(0);
     }
+
 }
